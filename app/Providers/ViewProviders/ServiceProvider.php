58c289182768ca915dfccc4180b50464a66c5338
группы стационара <st@itstep.lan>
2017-12-12 20:01:23 +0300

    sixth commit

---
+++ app/Providers/ViewProviders/ServiceProvider.php
@@ -0,0 +1,11 @@
+﻿<?php
+namespace App\Providers\ViewComposers;
+	class SiteComposer{
+		public function compose(View $view){
+			$ur = url()->full();
+			$arr = explode('/',$ur);
+			$end = end($arr);
+			$view->with('end',$end);
+			
+		}
+}
